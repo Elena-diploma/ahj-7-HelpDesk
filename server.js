0a2184394ef73703b7f3a8cc4c1065dc0ea30de8
Elena Tursunova <elenatursunova@gmail.com>
2022-02-20 12:49:15 -0400

    add 1

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const path = require('path');
+const Ticket = require('./ticket');
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const koaStatic = require('koa-static');
+const cors = require('@koa/cors');
+const app = new Koa();
+
+const publicDir = path.join(__dirname, '/public');
+app.use(cors());
+
+app.use(koaStatic(publicDir));
+
+app.use(koaBody({
+    urlencoded: true,
+    multipart:true,
+    text: true,
+    json: true,
+}));
+
+app.use(async (ctx) => {
+    const { method } = ctx.request;
+    switch (method) {
+        case "GET":
+            if (ctx.request.query.method === "allTickets") {
+                ctx.response.body = await Ticket.getAll();
+                ctx.response.status = 200;
+            } else if (ctx.request.query.method === "ticketById") {
+                ctx.response.body = await Ticket.getById(ctx.request.query.id);
+            }
+            break;
+        case "POST":
+            if (ctx.request.query.method === "allTickets") {
+                const {name, description} = ctx.request.body;
+                ctx.response.body = new Ticket(
+                    null,
+                    name,
+                    description,
+                    false,
+                    new Date()
+                );
+                ctx.response.status = 200;
+                return;
+            }
+            break;
+        default:
+            ctx.response.status = 404;
+            return;
+    }
+});
+
+const port = process.env.PORT || 7070;
+http.createServer(app.callback()).listen(port);
